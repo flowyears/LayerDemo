30026fd629861ec6d7d284677f8babd9f73e271f
flowyears <flowyears1225@gmail.com>
2015-03-04 16:05:43 +0800

    Initial Commit

---
+++ LayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LayerDemo
+//
+//  Created by 沈佳雄 on 15/3/4.
+//  Copyright (c) 2015年 shenjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LayerDemo
+//
+//  Created by 沈佳雄 on 15/3/4.
+//  Copyright (c) 2015年 shenjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
